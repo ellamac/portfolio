963f06af10eeaffb611f73f571a5d039bb14f1f0
Ella Mäkelä <ella@makela.co>
2023-11-20 14:34:11 +0200

    updated home page texts

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,14 @@ const About = (props) => {
       <h2>About me</h2>
       <p>
         In all my work, my goal is to create successful customer experiences. I
-        believe accessible systems, usable interfaces and user experiences
-        should be considered in all stages of a products life cycle. I am a
+        believe accessible systems, usable interfaces, and user experiences
+        should be considered in all stages of a product's life cycle. I am a
         master's graduate of cognitive science from the University of Jyväskylä.
         In my studies, I focused on user-centered design and user research. I
         further developed my programming skills from my undergrad in computer
         science by focusing on web development. I enjoy combining my
         understanding of both users and the tech stack. I believe bridging the
-        gap between users, product owners, designers and developers is crucial
+        gap between users, product owners, designers, and developers is crucial
         when building successful products and experiences!
       </p>
     </section>
