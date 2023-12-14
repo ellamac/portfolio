82ec598fb18c427936027214984be57e67a16c30
Ella Mäkelä <ella@makela.co>
2023-12-14 16:08:07 +0200

    langloc of cardnav

--- src/helpers/CardHelpers.js
+++ src/helpers/CardHelpers.js
@@ -1,12 +1,14 @@
+import getLanguage from '../data/GetLanguage';
+
 /* creates an array of objects that work with Card */
 export const createCardObjects = (arr) => {
   const newArr =
     arr.length > 0
       ? arr.map((o) => ({
-          mainimg: o.find(({ type }) => type === "mainimg").content,
-          header: o.find(({ type }) => type === "header").content,
-          intro: o.find(({ type }) => type === "intro").content,
-          name: o.find(({ type }) => type === "name").content,
+          mainimg: o.find(({ type }) => type === 'mainimg').content,
+          header: getLanguage(o.find(({ type }) => type === 'header')),
+          intro: getLanguage(o.find(({ type }) => type === 'intro')),
+          name: o.find(({ type }) => type === 'name').content,
         }))
       : [];
   return newArr;
