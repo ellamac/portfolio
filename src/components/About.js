8f8612f48ce7c928d05d4750620faea736356b63
Ella Mäkelä <ella@makela.co>
2025-09-12 23:07:32 +0300

    trying to fix broken homepage

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import MarkdownContent from '../data/MarkdownContent.js';
 const About = () => {
   return (
     <section className='about'>
-      <MarkdownContent path='portfolio/texts/about.md' />
+      <MarkdownContent path='/portfolio/texts/about.md' />
     </section>
   );
 };
--- src/components/Brief.js
+++ src/components/Brief.js
@@ -4,14 +4,11 @@ const Brief = () => {
   return (
     <section className='brief'>
       <figure className='pic'>
-        <img
-          src='https://res.cloudinary.com/dvvizrtil/image/upload/v1687773619/portfolio/ella2.jpg'
-          alt='My headshot'
-        />
+        <img src='/portfolio/public/ella2.jpg' alt='My headshot' />
       </figure>
 
       <main className='info'>
-        <MarkdownContent path='portfolio/texts/brief.md' />
+        <MarkdownContent path='/portfolio/texts/brief.md' />
       </main>
     </section>
   );
